894c079ee77fd4f632d62898defb9865dcfd83fd
xorond <xorond@gmail.com>
2016-10-05 21:26:19 +0300

    make mac seperate entry

--- setup.md.blade.php
+++ setup.md.blade.php
@@ -7,7 +7,9 @@
 
 2) Write the ISO to a USB Stick (8G or larger) or a CD-ROM.
 
-* Linux and Mac OS X: `dd if=ArchStrike-openbox-2016.07.21-x86_64.iso of=/dev/sdX bs=4M status=progress`
+* Linux: `dd if=ArchStrike-openbox-2016.07.21-x86_64.iso of=/dev/sdX bs=4M status=progress`
+
+* Mac: `dd if=ArchStrike-openbox-2016.07.21-x86_64.iso of=/dev/diskX bs=1`
 
 * Windows: Download [Rufus](https://rufus.akeo.ie/), select your USB device and the downloaded ISO file. **Important: Choose `DD image mode` on Rufus after you press the start button or the ISO won't boot.**
 
