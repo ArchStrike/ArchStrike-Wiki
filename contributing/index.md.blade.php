f796a3cf97a010091eb8f63ac766b1fb178a26d2
Kevin MacMartin <prurigro@gmail.com>
2016-11-29 19:54:51 -0500

    Create a sub-level for contributing

---
+++ contributing/index.md.blade.php
@@ -0,0 +1,7 @@
+# Contributing
+
+[Getting Involved](/wiki/contributing/involvement) - Information about getting involved in the project.
+
+[Contribution Details](/wiki/contributing/details) - Details explaining Pull Requests on GitHub.
+
+[Testing PKGBUILDs](/wiki/contributing/chroot) - A quick guide on putting together your chroot environment to test PKGBUILD files on.
--- index.md.blade.php
+++ index.md.blade.php
@@ -12,13 +12,13 @@ Welcome to the [ArchStrike Wiki](/wiki): your source for ArchStrike documentatio
 
 [Setting up ArchStrike Desktop Themes](/wiki/desktop) - How to set up the i3, openbox and xfce configuration files
 
-## Contributing
+## [Contributing](/wiki/contributing)
 
-[Getting Involved](/wiki/getinvolved) - Information about getting involved in the project.
+[Getting Involved](/wiki/contributing/involvement) - Information about getting involved in the project.
 
-[Contribution Details](/wiki/contributing) - Details explaining Pull Requests on GitHub.
+[Contribution Details](/wiki/contributing/details) - Details explaining Pull Requests on GitHub.
 
-[Testing PKGBUILDs](/wiki/chroot) - A quick guide on putting together your chroot environment to test PKGBUILD files on.
+[Testing PKGBUILDs](/wiki/contributing/chroot) - A quick guide on putting together your chroot environment to test PKGBUILD files on.
 
 ## [Tutorials](/wiki/tutorials)
 
