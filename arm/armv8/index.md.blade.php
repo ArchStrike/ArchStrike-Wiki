8e0bbf3c6312d91336579d7c8746a676b70b3ec4
xorond <xorond@gmail.com>
2016-12-03 02:23:50 +0300

    added armv8 guides

--- arm/armv8/index.md.blade.php
+++ arm/armv8/index.md.blade.php
@@ -1,1 +1,19 @@
 # ARMv8 Install Guides
+
+## Amlogic
+
+[ODROID-C2](/wiki/arm/armv8/odroid-c2)
+
+## ARM
+
+[Juno](/wiki/arm/armv8/juno)
+
+## Broadcom
+
+[Raspberry Pi 3](/wiki/arm/armv8/raspberry-pi-3)
+
+## Mediatek
+
+[Acer Chromebook R13](/wiki/arm/armv8/acer-chromebook-r13)
+
+## [Generic Installations](/wiki/arm/armv8/generic)
--- arm/index.md.blade.php
+++ arm/index.md.blade.php
@@ -6,4 +6,128 @@
 
 ## [ARMv7](/wiki/arm/armv7)
 
+### Allwinner
+
+[A10 OlinuXino Lime](/wiki/arm/armv7/a10-olinuxino-lime)
+
+[A20 OlinuXino Lime](/wiki/arm/armv7/a20-olinuxino-lime)
+
+[A20 OlinuXino Lime 2](/wiki/arm/armv7/a20-olinuxino-lime2)
+
+[A20 OlinuXino Micro](/wiki/arm/armv7/a20-olinuxino-micro)
+
+[Cubieboard](/wiki/arm/armv7/cubieboard)
+
+[Cubieboard 2](/wiki/arm/armv7/cubieboard-2)
+
+[Cubietruck](/wiki/arm/armv7/cubietruck)
+
+[pcDuino 3](/wiki/arm/armv7/pcduino3)
+
+### Amlogic
+
+[ODROID-C1](/wiki/arm/armv7/odroid-c1)
+
+### ARM
+
+[Versatile Express](/wiki/arm/armv7/versatile-express)
+
+### Broadcom
+
+[Raspberry Pi 2](/wiki/arm/armv7/raspberry-pi-2)
+
+### Freescale
+
+[CuBox-i](/wiki/arm/armv7/cubox-i)
+
+[Hobbitboard](/wiki/arm/armv7/hobbitboard)
+
+[Nitrogen6X](/wiki/arm/armv7/nitrogen6x)
+
+[SABRE Lite](/wiki/arm/armv7/sabre-lite)
+
+[UDOO](/wiki/arm/armv7/udoo)
+
+[USB Armory](/wiki/arm/armv7/usb-armory)
+
+[Utilite](/wiki/arm/armv7/utilite)
+
+[Wandboard](/wiki/arm/armv7/wandboard)
+
+### Marvell
+
+[ClearFog](/wiki/arm/armv7/clearfog)
+
+[CuBox](/wiki/arm/armv7/clearfog)
+
+[D3Plug](/wiki/arm/armv7/d3plug)
+
+[Mirabox](/wiki/arm/armv7/mirabox)
+
+[SMILE Plug](/wiki/arm/armv7/smile-plug)
+
+### nVidia
+
+[TrimSlice](/wiki/arm/armv7/trimslice)
+
+### Rockchip
+
+[Asus Chromebit CS10](/wiki/arm/armv7/asus-chromebit-cs10)
+
+[Asus Chromebook Flip C100P](/wiki/arm/armv7/asus-chromebook-flip-c100p)
+
+[Hisense Chromebook C11](/wiki/arm/armv7/hisense-chromebook-c11)
+
+### Samsung
+
+[ODROID-U2](/wiki/arm/armv7/odroid-u2)
+
+[ODROID-U3](/wiki/arm/armv7/odroid-u3)
+
+[ODROID-X](/wiki/arm/armv7/odroid-x)
+
+[ODROID-X2](/wiki/arm/armv7/odroid-x2)
+
+[ODROID-XU](/wiki/arm/armv7/odroid-xu)
+
+[ODROID-XU3](/wiki/arm/armv7/odroid-xu3)
+
+[ODROID-XU4](/wiki/arm/armv7/odroid-xu4)
+
+[Samsung Chromebook](/wiki/arm/armv7/samsung-chromebook)
+
+### TI
+
+[BeagleBoard](/wiki/arm/armv7/beagleboard)
+
+[BeagleBoard XM](/wiki/arm/armv7/beagleboard-xm)
+
+[BeagleBone](/wiki/arm/armv7/beaglebone)
+
+[BeagleBone Black](/wiki/arm/armv7/beaglebone-black)
+
+[ChiliBoard](/wiki/arm/armv7/chiliboard)
+
+[PandaBoard](/wiki/arm/armv7/pandaboard)
+
+[ZedBoard](/wiki/arm/armv7/zedboard)
+
 ## [ARMv8](/wiki/arm/armv8)
+
+### Amlogic
+
+[ODROID-C2](/wiki/arm/armv8/odroid-c2)
+
+### ARM
+
+[Juno](/wiki/arm/armv8/juno)
+
+### Broadcom
+
+[Raspberry Pi 3](/wiki/arm/armv8/raspberry-pi-3)
+
+### Mediatek
+
+[Acer Chromebook R13](/wiki/arm/armv8/acer-chromebook-r13)
+
+### [Generic Installations](/wiki/arm/armv8/generic)
