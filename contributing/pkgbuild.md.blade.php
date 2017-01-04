08c5a387e214d3328381b1ef21ee6ff671f208f0
xorond <xorond@gmail.com>
2017-01-04 13:10:49 +0300

    add some descriptions and change titles for the pkgbuild guide
    fix up some formatting
    
    add space

--- contributing/pkgbuild.md.blade.php
+++ contributing/pkgbuild.md.blade.php
@@ -69,6 +69,7 @@ The main function for packaging the source. Here we install the necessary files 
 ## Tips for PKGBUILD contributors
 
 * Run `namcap` on your `PKGBUILD` file and your `pkgname.tar.xz` after building it. This will uncover common errors. Such as `namcap some-package-x86_64.tar.xz` or `namcap PKGBUILD`.
+
   **You can ignore warnings about the buildarch variable not starting with an underscore**
 
 * Make sure your package works and installs properly by [testing your PKGBUILD in a clean chroot](/wiki/contributing/chroot).
